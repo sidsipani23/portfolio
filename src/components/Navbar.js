10014aa96863892180b0ece3bb46cdd354982252
Siddhartha <siddharthasip@gmail.com>
2020-12-06 00:20:30 +0530

    Minor bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import useThemeSwitcher from '../context/useThemeSwitcher';
 function Navbar() {
 	const [closed, setClosed] = useState(true);
